03c408c71efa9f6a4a8619b850470d8c6a394fb0
ssharma2003 <shreyasharma0020@gmail.com>
2024-05-02 18:00:04 +0530

    routing updated

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import { Route } from 'react-router-dom'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path= '/' element={<App/>}></Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
